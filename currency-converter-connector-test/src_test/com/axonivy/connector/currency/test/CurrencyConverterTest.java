f29f4f3e3fb43351e830dc2cbf11a72649270be9
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-07-18 16:11:10 +0700

    MARP-1983 add statelessTestsetReporter

--- currency-converter-connector-test/src_test/com/axonivy/connector/currency/test/CurrencyConverterTest.java
+++ currency-converter-connector-test/src_test/com/axonivy/connector/currency/test/CurrencyConverterTest.java
@@ -8,9 +8,9 @@ import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-import com.axonivy.connector.currency.constant.CurrencyConverterConstant;
+import com.axonivy.connector.currency.constant.CurrencyConverterTestConstant;
 import com.axonivy.connector.currency.context.MultiEnvironmentContextProvider;
-import com.axonivy.connector.currency.utils.CurrencyConverterUtils;
+import com.axonivy.connector.currency.utils.CurrencyConverterTestUtils;
 
 import app.frankfurter.api.client.CurCodes;
 import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
@@ -28,12 +28,12 @@ public class CurrencyConverterTest {
 
 	@BeforeEach
 	void beforeEach(ExtensionContext context, AppFixture fixture) {
-		CurrencyConverterUtils.setUpConfigForContext(context.getDisplayName(), fixture);
+		CurrencyConverterTestUtils.setUpConfigForContext(context.getDisplayName(), fixture);
 	}
 
 	@TestTemplate
 	void chfToEur(ExtensionContext context, BpmClient bpmClient) throws NoSuchFieldException {
-		boolean isRealContext = context.getDisplayName().equals(CurrencyConverterConstant.REAL_CALL_CONTEXT_DISPLAY_NAME);
+		boolean isRealContext = context.getDisplayName().equals(CurrencyConverterTestConstant.REAL_CALL_CONTEXT_DISPLAY_NAME);
 		BpmElement startable = testee.elementName("convert(Number,CurCodes,CurCodes)");
 		ExecutionResult result = bpmClient.start().subProcess(startable).execute(25.0, CurCodes.CHF, CurCodes.EUR);
 		if (isRealContext) {
