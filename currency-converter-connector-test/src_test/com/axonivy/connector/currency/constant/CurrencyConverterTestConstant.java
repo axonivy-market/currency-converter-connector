8fc024037de520bf5f2907715df33f3802beb985
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-07-30 16:53:47 +0700

    Add E2E workflow and test environment configuration
    Introduces a new GitHub Actions workflow for end-to-end (E2E) builds. Updates the test POM to pass the test environment as a system property. Adds constants for E2E environment keys and values, and modifies the context provider to select test environments based on the system property.

--- currency-converter-connector-test/src_test/com/axonivy/connector/currency/constant/CurrencyConverterTestConstant.java
+++ currency-converter-connector-test/src_test/com/axonivy/connector/currency/constant/CurrencyConverterTestConstant.java
@@ -3,4 +3,6 @@ package com.axonivy.connector.currency.constant;
 public class CurrencyConverterTestConstant {
 	public static final String MOCK_SERVER_CONTEXT_DISPLAY_NAME = "Mock Server Test";
 	public static final String REAL_CALL_CONTEXT_DISPLAY_NAME = "Real Server Test";
+	public static final String END_TO_END_TESTING_ENVIRONMENT_KEY = "testEnvironment";
+	public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E";
 }
