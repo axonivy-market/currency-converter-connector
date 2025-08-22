25e2e8ef81b53581ba55451958718b883412031d
Trần Vĩnh Thiện Phúc <143604440+tvtphuc-axonivy@users.noreply.github.com>
2025-08-22 16:29:27 +0700

    Merge pull request #50 from axonivy-market/feature/MARP-2631-Unify-code-for-e2e-testing-approach
    MARP-2631 Removed unused codes

--- currency-converter-connector-test/src_test/com/axonivy/connector/currency/test/CurrencyConverterTest.java
+++ currency-converter-connector-test/src_test/com/axonivy/connector/currency/test/CurrencyConverterTest.java
@@ -2,16 +2,15 @@ package com.axonivy.connector.currency.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static com.axonivy.utils.e2etest.enums.E2EEnvironment.REAL_SERVER;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.ExtensionContext;
-
-import com.axonivy.connector.currency.constant.CurrencyConverterTestConstant;
-import com.axonivy.connector.currency.context.MultiEnvironmentContextProvider;
-import com.axonivy.connector.currency.utils.CurrencyConverterTestUtils;
-
+import com.axonivy.connector.currency.CurrencyMock;
+import com.axonivy.utils.e2etest.context.MultiEnvironmentContextProvider;
+import com.axonivy.utils.e2etest.utils.E2ETestUtils;
 import app.frankfurter.api.client.CurCodes;
 import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
 import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
@@ -24,22 +23,33 @@ import ch.ivyteam.ivy.environment.AppFixture;
 @ExtendWith(MultiEnvironmentContextProvider.class)
 public class CurrencyConverterTest {
 
-	private static final BpmProcess testee = BpmProcess.path("CurrencyConverter");
+  private static final BpmProcess testee = BpmProcess.path("CurrencyConverter");
 
-	@BeforeEach
-	void beforeEach(ExtensionContext context, AppFixture fixture) {
-		CurrencyConverterTestUtils.setUpConfigForContext(context.getDisplayName(), fixture);
-	}
+  @BeforeEach
+  void beforeEach(ExtensionContext context, AppFixture fixture) {
+    E2ETestUtils.determineConfigForContext(context.getDisplayName(), runRealEnv(fixture), runMockEnv(fixture));
+  }
 
-	@TestTemplate
-	void chfToEur(ExtensionContext context, BpmClient bpmClient) throws NoSuchFieldException {
-		boolean isRealContext = context.getDisplayName().equals(CurrencyConverterTestConstant.REAL_CALL_CONTEXT_DISPLAY_NAME);
-		BpmElement startable = testee.elementName("convert(Number,CurCodes,CurCodes)");
-		ExecutionResult result = bpmClient.start().subProcess(startable).execute(25.0, CurCodes.CHF, CurCodes.EUR);
-		if (isRealContext) {
-			assertNotNull(result.data().last().get("ToAmount"));
-		} else {
-			assertThat(result.data().last().get("ToAmount")).isEqualTo(24.0);
-		}
-	}
+  private Runnable runRealEnv(AppFixture fixture) {
+    return () -> {
+    };
+  }
+
+  private Runnable runMockEnv(AppFixture fixture) {
+    return () -> {
+      fixture.config("RestClients.'Currency Converter API (CurrencyConverter)'.Url", CurrencyMock.URI);
+    };
+  }
+
+  @TestTemplate
+  void chfToEur(ExtensionContext context, BpmClient bpmClient) throws NoSuchFieldException {
+    boolean isRealContext = context.getDisplayName().equals(REAL_SERVER.getDisplayName());
+    BpmElement startable = testee.elementName("convert(Number,CurCodes,CurCodes)");
+    ExecutionResult result = bpmClient.start().subProcess(startable).execute(25.0, CurCodes.CHF, CurCodes.EUR);
+    if (isRealContext) {
+      assertNotNull(result.data().last().get("ToAmount"));
+    } else {
+      assertThat(result.data().last().get("ToAmount")).isEqualTo(24.0);
+    }
+  }
 }
